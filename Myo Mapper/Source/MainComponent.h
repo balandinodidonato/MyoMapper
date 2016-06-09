f47a9873360728a053338ea644f95f07ad0f2a4c
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2016-06-09 03:38:14 +0100

    deleted for loop for testing

--- Myo Mapper/Source/MainComponent.h
+++ Myo Mapper/Source/MainComponent.h
@@ -36,27 +36,15 @@ public:
     void paint (Graphics& g) override
     {
         g.fillAll(Colours::grey);
+        
+        emg.setValues(testEMG); // import emg <- sobstitute with EMG vector
+        orientation.setValues(testYaw, testPitch, testRoll); // <- sobstitute with yaw, pitch roll values
     }
 
     void resized() override
     {
         orientation.setBounds(10, getHeight()*0.19, getWidth()*0.5, getHeight()*0.8);
         emg.setBounds(getWidth()*0.5+20, getHeight()*0.19, getWidth()*0.5-30, getHeight()*0.8);
-        
-        // ===== TEST ====
-        int testEMG[8];
-        float testYaw = 0.2;
-        float testPitch = 0.5;
-        float testRoll = 0.8;
-
-        for(int i=0; i<8; i++)
-        {
-            testEMG[i] = i*10;
-        }
-        // ====== END TEST ===
-        
-        emg.setValues(testEMG); // import emg <- sobstitute with EMG vector
-        orientation.setValues(testYaw, testPitch, testRoll); // <- sobstitute with yaw, pitch roll values
         
         orientation.setOSCPort(9001);
         emg.setOSCPort(9001);
@@ -65,6 +53,14 @@ public:
 private:
     Orientation orientation;
     Emg emg;
+    
+    // ===== TEST data ====
+    int testEMG[8] = {10, 20, 30, 40, 50, 60, 70, 80};
+    float testYaw = 0.2;
+    float testPitch = 0.5;
+    float testRoll = 0.8;
+    // =================
+
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
