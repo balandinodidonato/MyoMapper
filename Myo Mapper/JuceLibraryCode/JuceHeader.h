a6d6968b802d64c42d67250866fe0bd5e44cf6a3
Jamie Bullock <jamie@jamiebullock.com>
2016-06-09 16:05:00 +0100

    Remove unused modules

--- Myo Mapper/JuceLibraryCode/JuceHeader.h
+++ Myo Mapper/JuceLibraryCode/JuceHeader.h
@@ -14,12 +14,7 @@
 #define __APPHEADERFILE_M7UYPJ__
 
 #include "AppConfig.h"
-#include "modules/juce_audio_basics/juce_audio_basics.h"
-#include "modules/juce_audio_devices/juce_audio_devices.h"
-#include "modules/juce_audio_formats/juce_audio_formats.h"
-#include "modules/juce_audio_processors/juce_audio_processors.h"
 #include "modules/juce_core/juce_core.h"
-#include "modules/juce_cryptography/juce_cryptography.h"
 #include "modules/juce_data_structures/juce_data_structures.h"
 #include "modules/juce_events/juce_events.h"
 #include "modules/juce_graphics/juce_graphics.h"
