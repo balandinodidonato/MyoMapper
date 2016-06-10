aeb9259b0b927609250b244dea42d6efff50027f
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2016-06-10 11:16:32 +0100

    GUI, gyro and acc window updated.

--- Myo Mapper/Source/MainComponent.h
+++ Myo Mapper/Source/MainComponent.h
@@ -30,10 +30,64 @@ public:
     MainComponent();
     ~MainComponent() {};
 
+<<<<<<< 60f9e6ee968f45512dde1de975fcc57d123f7d91
     void paint (Graphics& g) override;
     void resized() override;
     void sliderValueChanged (Slider* slider) override;
     void labelTextChanged (Label* labelThatHasChanged) override;
+=======
+    ~MainComponent()
+    {
+    }
+
+    void paint (Graphics& g) override
+    {
+        g.fillAll(Colours::grey);
+        
+        emg.setValues(testEMG); // int emg[8] <- sobstitute with EMG vector
+        orientation.setValues(testYaw, testPitch, testRoll); // <- sobstitute with (float) yaw, (float) pitch (float) roll values
+        gyro.setValues(testGYRO); // int gyro[3] <- sobstitute with Gyro vector
+        acc.setValues(testACC); // int acc[3] <- sobstitute with Acceleration vector
+    }
+
+    void resized() override
+    {
+        orientation.setBounds(10, getHeight()*0.19, (getWidth()*0.5)-15, (getHeight()*0.5)-15);
+        emg.setBounds(orientation.getRight()+10, orientation.getY(), orientation.getWidth(), orientation.getHeight());
+        
+        gyro.setBounds(10, getHeight()*0.68, getWidth()*0.28, (getHeight()*0.3));
+        acc.setBounds(gyro.getRight()+10, gyro.getY(), gyro.getWidth(), gyro.getHeight());
+        
+        settingsPannel.setBounds(10, 10, getRight()-20, getHeight()*0.19-10);
+        
+        oscPortSlider.setBounds(getX()+100, getY()+50, getWidth()*0.15, getHeight()*0.03);
+        
+        hostAddressTitle.setBounds(getX()+30, getY()+100, getWidth()*0.2, getHeight()*0.03);
+        setHostAddress.setBounds(getX()+125, getY()+100, getWidth()*0.14, getHeight()*0.03);
+    }
+    
+    void sliderValueChanged (Slider* slider) override
+    {
+        if (slider == &oscPortSlider)
+        {
+            orientation.setOSCPort(oscPortSlider.getValue());
+            emg.setOSCPort(oscPortSlider.getValue());
+            gyro.setOSCPort(oscPortSlider.getValue());
+            acc.setOSCPort(oscPortSlider.getValue());
+        }
+    }
+    
+    void labelTextChanged (Label* labelThatHasChanged)override
+    {
+        if (labelThatHasChanged == &setHostAddress)
+        {
+            orientation.setHostAddress(setHostAddress.getText());
+            emg.setOSChostAddress(setHostAddress.getText());
+            gyro.setOSChostAddress(setHostAddress.getText());
+            acc.setOSChostAddress(setHostAddress.getText());
+        }
+    }
+>>>>>>> GUI, gyro and acc window updated.
     
 private:
     Orientation orientation;
