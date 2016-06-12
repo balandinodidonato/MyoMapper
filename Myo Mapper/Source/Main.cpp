7cbeea5fd5b86f92c4bf979168f9a636ab0cad3d
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2016-06-12 14:39:05 +0100

    added disconnect myo function

--- Myo Mapper/Source/Main.cpp
+++ Myo Mapper/Source/Main.cpp
@@ -50,7 +50,6 @@ public:
         {
             setUsingNativeTitleBar (true);
             setContentOwned (new MainComponent(), true);
-
             setCentrePosition(getWidth()*0.5, 0);
             setVisible (true);
             setResizable(true, true);
@@ -59,18 +58,10 @@ public:
 
         void closeButtonPressed() override
         {
-            // This is called when the user tries to close this window. Here, we'll just
-            // ask the app to quit when this happens, but you can change this to do
-            // whatever you need.
+
+            MainComponent().disconnectMyo();
             JUCEApplication::getInstance()->systemRequestedQuit();
         }
-
-        /* Note: Be careful if you override any DocumentWindow methods - the base
-           class uses a lot of them, so by overriding you might break its functionality.
-           It's best to do all your work in your content component instead, but if
-           you really have to override any DocumentWindow methods, make sure your
-           subclass also calls the superclass's method.
-        */
 
     private:
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
