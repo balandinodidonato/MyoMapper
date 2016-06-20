df6bf7afeb4b1f816e7f1bd77dd133ca82ee5306
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2016-06-20 21:42:55 +0100

    About window updated

--- Myo Mapper/Source/AboutWindowContent.h
+++ Myo Mapper/Source/AboutWindowContent.h
@@ -1,63 +1,30 @@
 /*
-  ==============================================================================
-
-  This is an automatically generated GUI class created by the Projucer!
-
-  Be careful when adding custom code to these files, as only the code within
-  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
-  and re-saved.
-
-  Created with Projucer version: 4.2.1
-
-  ------------------------------------------------------------------------------
-
-  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
-  Copyright (c) 2015 - ROLI Ltd.
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ AboutWindow.cpp
+ Created: 20 Jun 2016 1:51:25pm
+ Author:  Balandino Di Donato
+ 
+ ==============================================================================
+ */
 
 #ifndef __JUCE_HEADER_CDC82D17D8FC0FC8__
 #define __JUCE_HEADER_CDC82D17D8FC0FC8__
 
-//[Headers]     -- You can add your own extra header files here --
 #include "../JuceLibraryCode/JuceHeader.h"
-//[/Headers]
 
 
-
-//==============================================================================
-/**
-                                                                    //[Comments]
-    An auto-generated component, created by the Projucer.
-
-    Describe your class and how it works here!
-                                                                    //[/Comments]
-*/
 class AboutWindowContent  : public Component
 {
 public:
-    //==============================================================================
     AboutWindowContent ();
     ~AboutWindowContent ();
-
-    //==============================================================================
-    //[UserMethods]     -- You can add your own custom methods in this section.
-    //[/UserMethods]
 
     void paint (Graphics& g) override;
     void resized() override;
 
-    // Binary resources:
-    static const char* icon_png;
-    static const int icon_pngSize;
-
-
 private:
-    //[UserVariables]   -- You can add your own custom variables in this section.
-    //[/UserVariables]
 
-    //==============================================================================
     ScopedPointer<Label> appName;
     ScopedPointer<ImageComponent> image;
     ScopedPointer<Label> version;
@@ -65,12 +32,7 @@ private:
     ScopedPointer<Label> date;
     ScopedPointer<HyperlinkButton> hyperlinkButton;
 
-
-    //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AboutWindowContent)
 };
 
-//[EndFile] You can add extra defines here...
-//[/EndFile]
-
-#endif   // __JUCE_HEADER_CDC82D17D8FC0FC8__
+#endif
