e622a1fe981482e2fe7a4ee6d1051a984fa0e9c4
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2016-11-08 12:22:21 +0000

    ZeroCrossing code added

--- Myo Mapper/Source/ZeroCrossing.cpp
+++ Myo Mapper/Source/ZeroCrossing.cpp
@@ -11,3 +11,25 @@ ZeroCrossing::ZeroCrossing()
 ZeroCrossing::~ZeroCrossing()
 {
 }
+
+void ZeroCrossing::setValue(float Value){
+    input = Value;
+    
+    if(index<window.size()){
+        window[index] = input;
+        index++;
+    }
+    else{
+        for(int i=1; i<window.size(); i++){
+            if((window[i]<=0 && window[i-1]>0) || (window[i]>0 && window[i-1]<=0)){
+                counter++;
+            }
+        }
+        zeroCrossingRate = counter/window.size();
+        index = 0;
+    }
+}
+
+float ZeroCrossing::getValue(){
+    return zeroCrossingRate;
+}
--- Myo Mapper/Source/ZeroCrossing.h
+++ Myo Mapper/Source/ZeroCrossing.h
@@ -4,14 +4,29 @@
 #define ZEROCROSSING_H_INCLUDED
 
 #include "../JuceLibraryCode/JuceHeader.h"
+#include <iostream>
+#include <array>
+
+using namespace std;
 
 class ZeroCrossing    : public Component
 {
 public:
     ZeroCrossing();
     ~ZeroCrossing();
+    
+    void setValue(float Value);
+    float getValue();
 
 private:
+
+    float input;
+    float zeroCrossingRate;
+    float output;
+    array<float,256> window;
+    int counter;
+    int index = 0;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ZeroCrossing)
 };
 
