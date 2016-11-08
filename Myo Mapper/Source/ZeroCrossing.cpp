0949c8babb02ebec45fc8bac4bfe60e7787cc3ec
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2016-11-08 11:36:33 +0000

    Zero Crossing class added

---
+++ Myo Mapper/Source/ZeroCrossing.cpp
@@ -0,0 +1,13 @@
+// Zero Crossing feature extractor
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "ZeroCrossing.h"
+
+ZeroCrossing::ZeroCrossing()
+{
+
+}
+
+ZeroCrossing::~ZeroCrossing()
+{
+}
---
+++ Myo Mapper/Source/ZeroCrossing.h
@@ -0,0 +1,19 @@
+// Zero Crossing feature extractor
+
+#ifndef ZEROCROSSING_H_INCLUDED
+#define ZEROCROSSING_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class ZeroCrossing    : public Component
+{
+public:
+    ZeroCrossing();
+    ~ZeroCrossing();
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ZeroCrossing)
+};
+
+
+#endif  // ZEROCROSSING_H_INCLUDED
