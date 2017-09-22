1c2e1f021ba033741647202f0a9eeb62ea7280f4
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2017-09-22 08:37:53 +0100

    fixes bugs - moving average

--- Myo Mapper/Source/MovingAverage.cpp
+++ Myo Mapper/Source/MovingAverage.cpp
@@ -12,14 +12,12 @@ MovingAverage::~MovingAverage()
 void MovingAverage::setValue(float Value, int NSamplesAvg)
 {
     nSamplesAvg = NSamplesAvg;
-    float samples[nSamplesAvg];
-    
-    samples[pointer] = Value;
+    input[pointer] = Value;
     
     sum = 0;
     
     for (int i=0; i<pointer; i++) {
-        sum = sum + samples[i];
+        sum = sum + input[i];
     }
     
     mavg = sum/nSamplesAvg;
@@ -36,17 +34,17 @@ float MovingAverage::getValue()
 void MovingAverage::setValue3D(Vector3D<float> Value3d, int NSamplesAvg3d)
 {
     nSamplesAvg3D = NSamplesAvg3d;
-    Vector3D<float> samples3d[nSamplesAvg3D];
-    
-    samples3d[pointer3d] = Value3d;
-    sum3d = {0, 0, 0};
+    input3d[pointer3d] = Value3d;
+
+    sum3d = {0, 0, 0,};
 
     for (int i=0; i<nSamplesAvg3D; i++) {
-        sum3d = sum3d + samples3d[i];
+        sum3d = sum3d + input3d[i];
     }
     
     mavg3d = sum3d/nSamplesAvg3D;
-    
+    std::cout << input3d[0].x << "+" << input3d[1].x << " = " << sum3d.x <<  " -  - mavg=" << mavg3d.x << std::endl;
+
     pointer3d++;
     pointer3d = pointer3d % nSamplesAvg3D;
 }
