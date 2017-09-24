d99167f260fb83f4a14f2a846d07580d0672e6df
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2017-09-24 10:56:20 +0100

    added min max feature

---
+++ Myo Mapper/Source/minMax.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class minMax
+{
+public:
+    
+    void setValues(float Values, int bufferSize);
+    void setValues(Vector3D<float> Values, int bufferSize);
+    float getMin();
+    float getMax();
+    Vector3D<float> getMin3D();
+    Vector3D<float> getMax3D();
+    
+private :
+    
+    int counter = 0;
+    float buffer[200];
+    float min = 100000;
+    float max = -100000;
+    
+    int counter3D = 0;
+    Vector3D<float> buffer3D[200];
+    Vector3D<float> min3D = {100000, 100000, 100000};
+    Vector3D<float> max3D = {-100000, -100000, -100000};
+};
