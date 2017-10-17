2dd85837d1d719aeb1a1937f47ec0267fddf8555
J-Bledsoe <jlbledsoe@hotmail.co.uk>
2017-10-17 12:07:47 +0100

    Created Class For OscBuffer
    Created a class to hold the variety of data and types to be sent via OSC

---
+++ Myo Mapper/Source/OscBuffer.h
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+    OscBuffer.h
+    Created: 16 Oct 2017 2:59:39pm
+    Author:  Jefferson Bledsoe
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class OscBuffer
+{
+public:
+    
+    
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OscBuffer);
+};
