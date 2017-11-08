2aa8e683a2a97ed66c4f3dcae2879c48d9a9ce7c
balandinodidonato <balandino.didonato@mail.bcu.ac.uk>
2017-11-08 23:16:02 +0000

    Pose label resized

--- Myo Mapper/Source/Pose.cpp
+++ Myo Mapper/Source/Pose.cpp
@@ -27,10 +27,10 @@ void Pose::paint (Graphics& g)
     g.fillAll (Colour::fromRGB (245, 245, 245));
     
     Path frontRect;
-    frontRect.addRoundedRectangle (area.removeFromBottom (proportionOfHeight (0.8)).reduced (proportionOfHeight (0.1)), cornerAndRoundness);
+    frontRect.addRoundedRectangle (area.removeFromBottom (proportionOfHeight (0.7)).reduced (proportionOfHeight (0.1)), cornerAndRoundness);
     
     auto rectangleCorner = area.getHeight() * 0.02;
-    auto lineThickness = area.getHeight() * 0.07;
+    auto lineThickness = area.getHeight() * 0.05;
     g.setColour (Colour::fromRGB (0, 129, 213));
     g.strokePath (frontRect, PathStrokeType (lineThickness));
 }
@@ -38,10 +38,9 @@ void Pose::paint (Graphics& g)
 void Pose::resized()
 {
     auto area = getLocalBounds();
-    titleLabel.setBounds (area.removeFromTop (proportionOfHeight (0.35))
-                          .reduced (0, proportionOfHeight (0.02)));
+    titleLabel.setBounds (area.removeFromTop (proportionOfHeight (0.45)).reduced (0, proportionOfHeight (0.02)));
     area.removeFromBottom (proportionOfHeight (0.1));
-    poseLabel.setBounds (area.reduced (0, proportionOfHeight (0.1)));
+    poseLabel.setBounds (area.reduced (0, proportionOfHeight (0.07)));
 }
 
 void Pose::setPoseLabel (String LabelText)
